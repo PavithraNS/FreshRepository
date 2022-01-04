d080cf0a4608328862b3d3cf89bbed08c9d9b29e
PavithraNS <nspavi1991@gmail.com>
2022-01-04 09:57:02 +0530

    [Pavithra] Modify fibonocci.cs

--- HelloWorldTest098/FibonocciSeries.cs
+++ HelloWorldTest098/FibonocciSeries.cs
@@ -16,28 +16,23 @@ namespace HelloWorldTest098
         /// instances variables 
         /// </summary>
         public int firstNum=0, secondNum=1, result=0;
-        //0 1 1 2 3
 
         public void FindFibonocciSeries()
         {
             Console.WriteLine("Please enter number to generate series");
-            //string num = Console.ReadLine();
+            //Reading input from user
             int num=Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("User number is: "+num);
             result = firstNum + secondNum;
+
             Console.Write("Series are:" + firstNum + " " + secondNum);
             for(int i=2;i<num;i++)
             {
                 firstNum = secondNum;
                 secondNum = result;
                 Console.Write(" " + result);
-                result = firstNum + secondNum;
-                
+                result = firstNum + secondNum; 
             }
             
-        }
-
-
-        
+        } 
     }
 }
